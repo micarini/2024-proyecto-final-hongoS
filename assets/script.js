f87f1eb4115ea88de584bfe9357d807442521bd7
micarini <miacarinirojo@gmail.com>
2024-11-12 23:11:32 -0300

    card de la shop q no anda!!

--- assets/script.js
+++ assets/script.js
@@ -179,7 +179,7 @@ for (let i = 0; i < productos4.length; i++) {
 }
 
 
-/*slider home*/
+/*SLIDER HOME*/
 const sliderContent = ['../assets/fotos/furniture-collection1.webp', '../assets/fotos/furniture-collection2.jpg', '../assets/fotos/furniture-collection3.jpg'];
 
 /* CONSIGNA: Escribir el código necesario para que al hacer click en su respectivo botón se circule la imagen de fondo del tag <main> con id "slider-background". Para esto deberán asignar eventos a los botones de la forma que prefieran, crear una función para iterar sobre el array "sliderContent" (si el usuario hace click para una dirección pero está en la posición final del array, la función deberá volver al primer item y viceversa) y editar el la propiedad "background-image" de nuestro <main> con id "slider-background" */
@@ -219,7 +219,64 @@ document.getElementById('01-button').addEventListener("click", goToImage01());
 document.getElementById('02-button').addEventListener("click", goToImage02());
 document.getElementById('03-button').addEventListener("click", goToImage03());
 
-/*slider blog*/
+/* GALERIA SHOP CONTAINER */
+
+//guardo el body en una variable
+const shopContainer = document.querySelector("#shop-container");
+
+let gridShop = document.createElement("div");
+gridShop.setAttribute("id", "gridShop"); 
+shopContainer.appendChild(gridShop);
+
+let productosShop = [
+    { titulo: "Zinnia Lamp", precio: "£150.00", img: "assets/fotos/foto1-galeria2.webp", imgHover: "assets/fotos/foto1-galeria2-imghover.webp" },
+    { titulo: "Black Wood Lamp", precio: "£2500.00", img: "assets/fotos/foto2-galeria2.webp", imgHover: "assets/fotos/foto2-galeria2-imghover.webp" },
+    { titulo: "White Metal Lamp", precio: "£150.00", img: "assets/fotos/foto3-galeria2.webp", imgHover: "assets/fotos/foto3-galeria2-imghover.webp" },
+    { titulo: "Brown Crystal Vase", precio: "£250.00", img: "assets/fotos/foto4-galeria2.webp", imgHover: "assets/fotos/foto4-galeria2-imghover.webp" },
+    { titulo: "Aether Vase", precio: "£600.00", img: "assets/fotos/foto5-galeria2.webp", imgHover: "assets/fotos/foto5-galeria2-imghover.webp" },
+    { titulo: "Antic Brown Vase", precio: "£480.00", img: "assets/fotos/foto6-galeria2.webp", imgHover: "assets/fotos/foto6-galeria2-imghover.webp" },
+    { titulo: "Harrington Style Stool", precio: "£320.00", img: "assets/fotos/foto7-galeria2.webp", imgHover: "assets/fotos/foto7-galeria2-imghover.webp" },
+    { titulo: "Fabric Table Lamp", precio: "£600.00", img: "assets/fotos/foto8-galeria2.webp", imgHover: "assets/fotos/foto8-galeria2-imghover.webp" }
+];
+
+//ahora tengo que crear la card y que esta se replique 16 veces
+//voy a hacer un for para meter adentro de esa card un titulo y una imagen
+
+let cardShop;
+for (let i = 0; i < productosShop.length; i++) {
+    cardShop = document.createElement("div"); 
+    cardShop.setAttribute("class", "cardShop"); 
+    
+    let titulo = document.createElement("h3");
+    titulo.setAttribute("class", "titulo"); 
+    titulo.textContent = productosShop[i].titulo; 
+
+    let img = document.createElement("img");
+    img.setAttribute("src", productosShop[i].img); 
+    img.setAttribute("class", "foto"); 
+
+    let precio = document.createElement("p");
+    precio.setAttribute("class", "precio"); 
+    precio.textContent = productosShp[i].precio; 
+
+    cardShop.appendChild(img);
+    cardShop.appendChild(titulo);
+    cardShop.appendChild(precio)
+
+    gridShop.appendChild(cardShop); 
+
+    //Evento mouseover para cambiar la imagen
+    img.addEventListener("mouseover", function () {
+        img.setAttribute("src", productosShop[i].imgHover);
+    });
+
+    // Evento mouseout para restaurar la imagen original
+    img.addEventListener("mouseout", function () {
+        img.setAttribute("src", productosShop[i].img);
+    });
+}
+
+/*SLIDER BLOG*/
 const sliderBlog = [
     '../assets/fotos/blog-slider-1.webp',
     '../assets/fotos/blog-slider-2.webp',
