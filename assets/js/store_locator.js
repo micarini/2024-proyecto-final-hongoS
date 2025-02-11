9b3c6dcf5160a64d6d30111c9822e249d56ea4ca
lolitaamazza <lolamazzaoliver@gmail.com>
2025-02-11 11:22:07 -0300

    accordion

--- assets/js/store_locator.js
+++ assets/js/store_locator.js
@@ -1,40 +1,40 @@
-/*accordion store locator*/
-let acc = document.getElementsByClassName("accordion");
-
-function funAccordion() {
-  // Contenidos para los paneles, definidos en JS (si los necesitas más adelante)
-  const panelContents = [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since.",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since."
-  ];
-
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      console.log("Accordion clicked:", this);
-
-      // Verifica si el acordeón clicado ya estaba activo
-      const isActive = this.classList.contains("active");
-
-      // Cierra todos los paneles
-      for (let j = 0; j < acc.length; j++) {
-        let panel = acc[j].nextElementSibling;
-        acc[j].classList.remove("active");
-        panel.style.maxHeight = "0"; // Cerrar todos los paneles
-      }
-
-      // Si no estaba activo, abrir este y agregar su contenido
-      if (!isActive) {
-        this.classList.add("active");
-        let panel = this.nextElementSibling;
-        panel.style.maxHeight = panel.scrollHeight + "px"; // Abrir panel
-      }
-    });
+if(document.getElementsByClassName("accordion")){
+  function funAccordion() {
+    let acc = document.getElementsByClassName("accordion");
+  
+    for (let i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        console.log("Accordion clicked:", this);
+  
+        const isActive = this.classList.contains("active");
+  
+        // Cierra todos los paneles
+        for (let j = 0; j < acc.length; j++) {
+          let panel = acc[j].nextElementSibling;
+          acc[j].classList.remove("active");
+          panel.style.maxHeight = null;
+          panel.style.display = "none"; // Oculta el panel cuando se cierra
+        }
+  
+        // Si no estaba activo, abrir este y mostrar el contenido
+        if (!isActive) {
+          this.classList.add("active");
+          let panel = this.nextElementSibling;
+          panel.style.display = "block"; // Asegura que el panel se muestre antes de calcular el alto
+          setTimeout(() => {
+            panel.style.maxHeight = panel.scrollHeight + "px"; // Expande el panel
+          }, 10); // Pequeño retraso para que el navegador detecte el cambio
+        }
+      });
+    }
   }
+  
+  // Ejecutar la función cuando el DOM haya cargado
+  document.addEventListener("DOMContentLoaded", function () {
+    funAccordion();
+  });
+  
 }
-
-// Llama la función para asignar los eventos de clic
-funAccordion();
 
 document.addEventListener("DOMContentLoaded", function () {
   // Seleccionamos el formulario del footer
