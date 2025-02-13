345cad931fa530293d74265a67b17e7576031f77
lolitaamazza <lolamazzaoliver@gmail.com>
2025-02-12 21:42:03 -0300

    intente poner los icons en la otta grid y no me sale:(

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ if (document.getElementById("slider")) {
     ];
 
     let currentIndex = 0;
-    const foto = document.getElementById('slider'); 
+    const foto = document.getElementById('slider');
     const titulo = document.querySelector('#slider h2'); // selecciona el h2 dentro del slider
     const button1 = document.getElementById('button1');
     const button2 = document.getElementById('button2');
@@ -74,7 +74,7 @@ if (document.getElementById("slider")) {
         updateSlider();
         resetAutoSlide();
     }
-    
+
     // Asignar eventos a los botones
     document.getElementById('button1').addEventListener("click", goToImage01);
     document.getElementById('button2').addEventListener("click", goToImage02);
@@ -84,7 +84,7 @@ if (document.getElementById("slider")) {
     updateSlider();
 }
 
-    if (document.getElementById("section2")) {
+if (document.getElementById("section2")) {
     const section2 = document.getElementById("section2");
 
     function fadeInOnScroll() {
@@ -186,40 +186,40 @@ if (document.getElementById("section3")) {
         seccion3.appendChild(grid);
     }
 
-       
-// Datos de productos
-let productos1 = [
-    { titulo: "Charcoal Linen Stool", precio: "£510.00", img: "assets/fotos/foto1-galeria1.webp", imgHover: "assets/fotos/foto1-galeria1-imghover.webp" },
-    { titulo: "Elton Arm Chair", precio: "£50.00", img: "assets/fotos/foto2-galeria1.webp", imgHover: "assets/fotos/foto2-galeria1-imghover.webp" },
-    { titulo: "Valeria ArmChair", precio: "£20.00 - £50.00", img: "assets/fotos/foto3-galeria1.webp", imgHover: "assets/fotos/foto3-galeria1-imghover.webp" },
-    { titulo: "Sampson Wood Chair", precio: "£200.00", img: "assets/fotos/foto4-galeria1.webp", imgHover: "assets/fotos/foto4-galeria1-imghover.webp" },
-    { titulo: "Round Coffee Table", precio: "£265.00", img: "assets/fotos/foto5-galeria1.webp", imgHover: "assets/fotos/foto5-galeria1-imghover.webp" },
-    { titulo: "Wooden Dining Chair", precio: "£255.00", img: "assets/fotos/foto6-galeria1.webp", imgHover: "assets/fotos/foto6-galeria1-imghover.webp" },
-    { titulo: "Wood Mini Stool", precio: "£255.00", img: "assets/fotos/foto7-galeria1.webp", imgHover: "assets/fotos/foto7-galeria1-imghover.webp" },
-    { titulo: "Classic Chairs", precio: "£255.00", img: "assets/fotos/foto8-galeria1.webp", imgHover: "assets/fotos/foto8-galeria1-imghover.webp" },
-];
 
-let productos2 = [
-    { titulo: "Antique Wood Chair", precioOriginal: "£150.00", precio: "£130.00", img: "assets/fotos/foto1-featuredp.webp", imgHover: "assets/fotos/foto1-featuredp-imghover.webp" },
-    { titulo: "Paige Backless Stool", precioOriginal: "£10.00 – £100.00", precio: "£10.00 – £100.00", img: "assets/fotos/foto2-featuredp.webp", imgHover: "assets/fotos/foto2-featuredp-imghover.webp" },
-    { titulo: "Viola Table Lamp", precioOriginal: "£50.00", precio: "£25.00", img: "assets/fotos/foto3-featuredp.webp", imgHover: "assets/fotos/foto3-featuredp-imghover.webp" },
-    { titulo: "Grey Brown Chair", precioOriginal: "£180.00", precio: "£150.00", img: "assets/fotos/foto4-featuredp.webp", imgHover: "assets/fotos/foto4-featuredp-imghover.webp" },
-    { titulo: "Lounge Accent Chair", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto5-featuredp.webp", imgHover: "assets/fotos/foto5-featuredp-imghover.webp" },
-    { titulo: "Wood Mini Stool", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto6-featuredp.webp", imgHover: "assets/fotos/foto6-featuredp-imghover.webp" },
-    { titulo: "Wooden Dining Chair", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto7-featuredp.webp", imgHover: "assets/fotos/foto7-featuredp-imghover.webp" },
-    { titulo: "Round Coffee Table", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto8-featuredp.webp", imgHover: "assets/fotos/foto8-featuredp-imghover.webp" }
-];
+    // Datos de productos
+    let productos1 = [
+        { titulo: "Charcoal Linen Stool", precio: "£510.00", img: "assets/fotos/foto1-galeria1.webp", imgHover: "assets/fotos/foto1-galeria1-imghover.webp" },
+        { titulo: "Elton Arm Chair", precio: "£50.00", img: "assets/fotos/foto2-galeria1.webp", imgHover: "assets/fotos/foto2-galeria1-imghover.webp" },
+        { titulo: "Valeria ArmChair", precio: "£20.00 - £50.00", img: "assets/fotos/foto3-galeria1.webp", imgHover: "assets/fotos/foto3-galeria1-imghover.webp" },
+        { titulo: "Sampson Wood Chair", precio: "£200.00", img: "assets/fotos/foto4-galeria1.webp", imgHover: "assets/fotos/foto4-galeria1-imghover.webp" },
+        { titulo: "Round Coffee Table", precio: "£265.00", img: "assets/fotos/foto5-galeria1.webp", imgHover: "assets/fotos/foto5-galeria1-imghover.webp" },
+        { titulo: "Wooden Dining Chair", precio: "£255.00", img: "assets/fotos/foto6-galeria1.webp", imgHover: "assets/fotos/foto6-galeria1-imghover.webp" },
+        { titulo: "Wood Mini Stool", precio: "£255.00", img: "assets/fotos/foto7-galeria1.webp", imgHover: "assets/fotos/foto7-galeria1-imghover.webp" },
+        { titulo: "Classic Chairs", precio: "£255.00", img: "assets/fotos/foto8-galeria1.webp", imgHover: "assets/fotos/foto8-galeria1-imghover.webp" },
+    ];
 
-let productos3 = [
-    { titulo: "Black Brown Chair", precioOriginal: "£180.00", precio: "£150.00", img: "assets/fotos/foto1-bests.webp", imgHover: "assets/fotos/foto1-bests-imghover.webp" },
-    { titulo: "Coal Paige Stool", precioOriginal: "£50.00", precio: "£25.00", img: "assets/fotos/foto2-bests.webp", imgHover: "assets/fotos/foto2-bests-imghover.webp" },
-    { titulo: "Normal Wood Chair", precioOriginal: "£10.00 – £100.00", precio: "£10.00 – £100.00", img: "assets/fotos/foto3-bests.webp", imgHover: "assets/fotos/foto3-bests-imghover.webp" },
-    { titulo: "Oscar Wood Stool", precioOriginal: "£150.00", precio: "£130.00", img: "assets/fotos/foto4-bests.webp", imgHover: "assets/fotos/foto4-bests-imghover.webp" },
-    { titulo: "Sitting Chair", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto5-bests.webp", imgHover: "assets/fotos/foto5-bests-imghover.webp" },
-    { titulo: "Outdoor Armchair", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto6-bests.webp", imgHover: "assets/fotos/foto6-bests-imghover.webp" },
-    { titulo: "Wooden Living Stool", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto7-bests.webp", imgHover: "assets/fotos/foto7-bests-imghover.webp" },
-    { titulo: "Bar Stools", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto8-bests.webp", imgHover: "assets/fotos/foto8-bests-imghover.webp" }
-];
+    let productos2 = [
+        { titulo: "Antique Wood Chair", precioOriginal: "£150.00", precio: "£130.00", img: "assets/fotos/foto1-featuredp.webp", imgHover: "assets/fotos/foto1-featuredp-imghover.webp" },
+        { titulo: "Paige Backless Stool", precioOriginal: "£10.00 – £100.00", precio: "£10.00 – £100.00", img: "assets/fotos/foto2-featuredp.webp", imgHover: "assets/fotos/foto2-featuredp-imghover.webp" },
+        { titulo: "Viola Table Lamp", precioOriginal: "£50.00", precio: "£25.00", img: "assets/fotos/foto3-featuredp.webp", imgHover: "assets/fotos/foto3-featuredp-imghover.webp" },
+        { titulo: "Grey Brown Chair", precioOriginal: "£180.00", precio: "£150.00", img: "assets/fotos/foto4-featuredp.webp", imgHover: "assets/fotos/foto4-featuredp-imghover.webp" },
+        { titulo: "Lounge Accent Chair", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto5-featuredp.webp", imgHover: "assets/fotos/foto5-featuredp-imghover.webp" },
+        { titulo: "Wood Mini Stool", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto6-featuredp.webp", imgHover: "assets/fotos/foto6-featuredp-imghover.webp" },
+        { titulo: "Wooden Dining Chair", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto7-featuredp.webp", imgHover: "assets/fotos/foto7-featuredp-imghover.webp" },
+        { titulo: "Round Coffee Table", precioOriginal: "£240.00", precio: "£220.00", img: "assets/fotos/foto8-featuredp.webp", imgHover: "assets/fotos/foto8-featuredp-imghover.webp" }
+    ];
+
+    let productos3 = [
+        { titulo: "Black Brown Chair", precioOriginal: "£180.00", precio: "£150.00", img: "assets/fotos/foto1-bests.webp", imgHover: "assets/fotos/foto1-bests-imghover.webp" },
+        { titulo: "Coal Paige Stool", precioOriginal: "£50.00", precio: "£25.00", img: "assets/fotos/foto2-bests.webp", imgHover: "assets/fotos/foto2-bests-imghover.webp" },
+        { titulo: "Normal Wood Chair", precioOriginal: "£10.00 – £100.00", precio: "£10.00 – £100.00", img: "assets/fotos/foto3-bests.webp", imgHover: "assets/fotos/foto3-bests-imghover.webp" },
+        { titulo: "Oscar Wood Stool", precioOriginal: "£150.00", precio: "£130.00", img: "assets/fotos/foto4-bests.webp", imgHover: "assets/fotos/foto4-bests-imghover.webp" },
+        { titulo: "Sitting Chair", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto5-bests.webp", imgHover: "assets/fotos/foto5-bests-imghover.webp" },
+        { titulo: "Outdoor Armchair", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto6-bests.webp", imgHover: "assets/fotos/foto6-bests-imghover.webp" },
+        { titulo: "Wooden Living Stool", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto7-bests.webp", imgHover: "assets/fotos/foto7-bests-imghover.webp" },
+        { titulo: "Bar Stools", precioOriginal: "", precio: "£255.00", img: "assets/fotos/foto8-bests.webp", imgHover: "assets/fotos/foto8-bests-imghover.webp" }
+    ];
 
     // Crear las tres galerías
     crearGaleria(productos1, "grid1");
@@ -268,70 +268,94 @@ let productos3 = [
 
 //guardo el body en una variable
 
-if(document.getElementById("section5")){
-const seccion5 = document.querySelector("#section5");
+if (document.getElementById("section5")) {
+    const seccion5 = document.querySelector("#section5");
 
-let h4_seccion5 = document.createElement("h4");
-h4_seccion5.textContent = "NEW PRODUCTS";
-seccion5.appendChild(h4_seccion5);
+    let h4_seccion5 = document.createElement("h4");
+    h4_seccion5.textContent = "NEW PRODUCTS";
+    seccion5.appendChild(h4_seccion5);
 
-let grid4 = document.createElement("div");
-//tengo que ponerle un id a mi contenedor grid
-grid4.setAttribute("id", "grid4"); //cambiandole el atributo id por el que quiero
-seccion5.appendChild(grid4);//inserto en el body mi grid
+    let grid4 = document.createElement("div");
+    //tengo que ponerle un id a mi contenedor grid
+    grid4.setAttribute("id", "grid4"); //cambiandole el atributo id por el que quiero
+    seccion5.appendChild(grid4);//inserto en el body mi grid
 
-let productos4 = [
-    { titulo: "Zinnia Lamp", precio: "£150.00", img: "assets/fotos/foto1-galeria2.webp", imgHover: "assets/fotos/foto1-galeria2-imghover.webp" },
-    { titulo: "Black Wood Lamp", precio: "£2500.00", img: "assets/fotos/foto2-galeria2.webp", imgHover: "assets/fotos/foto2-galeria2-imghover.webp" },
-    { titulo: "White Metal Lamp", precio: "£150.00", img: "assets/fotos/foto3-galeria2.webp", imgHover: "assets/fotos/foto3-galeria2-imghover.webp" },
-    { titulo: "Brown Crystal Vase", precio: "£250.00", img: "assets/fotos/foto4-galeria2.webp", imgHover: "assets/fotos/foto4-galeria2-imghover.webp" },
-    { titulo: "Aether Vase", precio: "£600.00", img: "assets/fotos/foto5-galeria2.webp", imgHover: "assets/fotos/foto5-galeria2-imghover.webp" },
-    { titulo: "Antic Brown Vase", precio: "£480.00", img: "assets/fotos/foto6-galeria2.webp", imgHover: "assets/fotos/foto6-galeria2-imghover.webp" },
-    { titulo: "Harrington Style Stool", precio: "£320.00", img: "assets/fotos/foto7-galeria2.webp", imgHover: "assets/fotos/foto7-galeria2-imghover.webp" },
-    { titulo: "Fabric Table Lamp", precio: "£600.00", img: "assets/fotos/foto8-galeria2.webp", imgHover: "assets/fotos/foto8-galeria2-imghover.webp" }
-];
+    let productos4 = [
+        { titulo: "Zinnia Lamp", precio: "£150.00", img: "assets/fotos/foto1-galeria2.webp", imgHover: "assets/fotos/foto1-galeria2-imghover.webp" },
+        { titulo: "Black Wood Lamp", precio: "£2500.00", img: "assets/fotos/foto2-galeria2.webp", imgHover: "assets/fotos/foto2-galeria2-imghover.webp" },
+        { titulo: "White Metal Lamp", precio: "£150.00", img: "assets/fotos/foto3-galeria2.webp", imgHover: "assets/fotos/foto3-galeria2-imghover.webp" },
+        { titulo: "Brown Crystal Vase", precio: "£250.00", img: "assets/fotos/foto4-galeria2.webp", imgHover: "assets/fotos/foto4-galeria2-imghover.webp" },
+        { titulo: "Aether Vase", precio: "£600.00", img: "assets/fotos/foto5-galeria2.webp", imgHover: "assets/fotos/foto5-galeria2-imghover.webp" },
+        { titulo: "Antic Brown Vase", precio: "£480.00", img: "assets/fotos/foto6-galeria2.webp", imgHover: "assets/fotos/foto6-galeria2-imghover.webp" },
+        { titulo: "Harrington Style Stool", precio: "£320.00", img: "assets/fotos/foto7-galeria2.webp", imgHover: "assets/fotos/foto7-galeria2-imghover.webp" },
+        { titulo: "Fabric Table Lamp", precio: "£600.00", img: "assets/fotos/foto8-galeria2.webp", imgHover: "assets/fotos/foto8-galeria2-imghover.webp" }
+    ];
 
-//ahora tengo que crear la card y que esta se replique 16 veces
-//voy a hacer un for para meter adentro de esa card un titulo y una imagen
+    //ahora tengo que crear la card y que esta se replique 16 veces
+    //voy a hacer un for para meter adentro de esa card un titulo y una imagen
 
-let card4;
-for (let i = 0; i < productos4.length; i++) {
-    card4 = document.createElement("div"); //creo el contenedor que sera la card2
-    card4.setAttribute("class", "card4"); //le agrego la clase card2 para poder modificarlas todas juntas
+    let card4 = document.querySelectorAll('card4');
+    for (let i = 0; i < productos4.length; i++) {
+        card4 = document.createElement("div"); //creo el contenedor que sera la card2
+        card4.setAttribute("class", "card4"); //le agrego la clase card2 para poder modificarlas todas juntas
 
-    //creo el titulo
-    let titulo = document.createElement("h3");
-    titulo.setAttribute("class", "titulo"); // le agrego la clase para despues manipularlo todo junto
-    titulo.textContent = productos4[i].titulo; //agrego el titulo por medio del array
+        //creo el titulo
+        let titulo = document.createElement("h3");
+        titulo.setAttribute("class", "titulo"); // le agrego la clase para despues manipularlo todo junto
+        titulo.textContent = productos4[i].titulo; //agrego el titulo por medio del array
 
-    //creo el elemento imagen
-    let img = document.createElement("img");
-    img.setAttribute("src", productos4[i].img); // Usar la ruta de imagen desde el array
-    img.setAttribute("class", "foto"); // Le agrego una clase para manipularlas juntas
+        //creo el elemento imagen
+        let img = document.createElement("img");
+        img.setAttribute("src", productos4[i].img); // Usar la ruta de imagen desde el array
+        img.setAttribute("class", "foto"); // Le agrego una clase para manipularlas juntas
 
-    // Crear el elemento precio
-    let precio = document.createElement("p");
-    precio.setAttribute("class", "precio"); // Clase para estilizar después
-    precio.textContent = productos4[i].precio; // Agregar el precio desde el array
+        // Crear el elemento precio
+        let precio = document.createElement("p");
+        precio.setAttribute("class", "precio"); // Clase para estilizar después
+        precio.textContent = productos4[i].precio; // Agregar el precio desde el array
 
-    card4.appendChild(img);
-    card4.appendChild(titulo);
-    card4.appendChild(precio)
+        //creo los iconos
+        let iconContainer = document.createElement("div");
+        iconContainer.className = "card-icons";
 
-    //ahora agarro el grid y le pongo las cards
-    grid4.appendChild(card4); //esta accion tiene que estar adentro del for para que por cada vuelta se inserte la card de cada vuelta
+        //iconos
+        let iconCart = document.createElement("i");
+        iconCart.className = "fa-solid fa-cart-shopping"
+        iconCart.title = "Add to Cart";
 
-    //Evento mouseover para cambiar la imagen
-    img.addEventListener("mouseover", function () {
-        img.setAttribute("src", productos4[i].imgHover);
-    });
+        let iconHeart = document.createElement("i");
+        iconHeart.className = "fa-solid fa-heart";
+        iconHeart.title = "Add to Wishlist";
 
-    // Evento mouseout para restaurar la imagen original
-    img.addEventListener("mouseout", function () {
-        img.setAttribute("src", productos4[i].img);
-    });
+        let iconEye = document.createElement("i");
+        iconEye.className = "fa-solid fa-eye";
+        iconEye.title = "Quick View";
+
+        iconContainer.appendChild(iconCart);
+        iconContainer.appendChild(iconHeart);
+        iconContainer.appendChild(iconEye);
+
+        card4.appendChild(img);
+        card4.appendChild(titulo);
+        card4.appendChild(precio);
+        card4.appendChild(iconContainer); //agrega los iconos a la tarjeta
+
+        //ahora agarro el grid y le pongo las cards
+        grid4.appendChild(card4); //esta accion tiene que estar adentro del for para que por cada vuelta se inserte la card de cada vuelta
+
+        //Evento mouseover para cambiar la imagen
+        img.addEventListener("mouseover", function () {
+            img.setAttribute("src", productos4[i].imgHover);
+        });
+
+        // Evento mouseout para restaurar la imagen original
+        img.addEventListener("mouseout", function () {
+            img.setAttribute("src", productos4[i].img);
+        });
+    }
 }
-}
+
+
 if (document.getElementsByClassName("gallery")) {
     const galleryItems = [
         { url: "https://www.instagram.com/p/B2ByAQ8Hv-9/", imgSrc: "../2024-proyecto-final-hongo/assets/fotos/cactus-almohadon.jpg", alt: "cactus-almohadon" },
@@ -343,22 +367,22 @@ if (document.getElementsByClassName("gallery")) {
         { url: "https://www.instagram.com/p/B2Bt1heHo4O/", imgSrc: "../2024-proyecto-final-hongo/assets/fotos/silla-living.jpg", alt: "silla-living" },
         { url: "https://www.instagram.com/p/B2BtXaRH50I/", imgSrc: "../2024-proyecto-final-hongo/assets/fotos/escritorio.jpg", alt: "escritorio" }
     ];
-    
+
     const galleryContainer = document.querySelector(".gallery");
 
     for (let i = 0; i < galleryItems.length; i++) {
-    const item = galleryItems[i];
-    const link = document.createElement("a");
-    link.className = item.class;
-    link.href = item.url;
-    
-    const img = document.createElement("img");
-    img.src = item.imgSrc;
-    img.alt = item.alt;
-    
-    link.appendChild(img);
-    galleryContainer.appendChild(link);
-}
+        const item = galleryItems[i];
+        const link = document.createElement("a");
+        link.className = item.class;
+        link.href = item.url;
+
+        const img = document.createElement("img");
+        img.src = item.imgSrc;
+        img.alt = item.alt;
+
+        link.appendChild(img);
+        galleryContainer.appendChild(link);
+    }
 
 }
 
